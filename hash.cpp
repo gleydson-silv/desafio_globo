9760930b8790c2f772a27afa316ba3b5880d9761
Gleydson Luidy <gleydsonluidy2@gmail.com>
2025-11-18 13:52:21 -0300

    Implementa medição de tempo em milissegundos

--- hash.cpp
+++ hash.cpp
@@ -16,7 +16,7 @@ long long funcao_hash(long long n) {
 
     if (n == 1) resultado = 1;
     else if (n % 2 == 0)
-        resultado = 1 + funcao_hash_hash(n / 2);
+        resultado = 1 + funcao_hash(n / 2);
     else
         resultado = 1 + funcao_hash(3*n + 1);
 
@@ -28,12 +28,13 @@ long long funcao_hash(long long n) {
 
 int main() {
     auto inicio = chrono::high_resolution_clock::now();
-
+    auto comeco = chrono::high_resolution_clock::now();
+    
     long long maior_numero = 1;
     long long maior_tamanho = 1;
 
     for (long long i = 1; i < 1000000; i++) {
-        long long tamanho_atual = collatz_hash(i);
+        long long tamanho_atual = funcao_hash(i);
         if (tamanho_atual > maior_tamanho) {
             maior_tamanho = tamanho_atual;
             maior_numero = i;
@@ -43,9 +44,13 @@ int main() {
     cout << "Tamanho da sequencia: " << maior_tamanho << endl;
 
     auto fim = chrono::high_resolution_clock::now();
+    auto termino = chrono::high_resolution_clock::now();
 
     chrono::duration<double> duracao = fim - inicio;
     cout << "Tempo: " << duracao.count() << " segundos\n";
+
+    auto duracao2 = chrono::duration_cast<chrono::milliseconds>(termini - comeco).count();
+    cout << "Tempo: " << duracao2 << "Milissegundos\n";
     
 
     return 0;
